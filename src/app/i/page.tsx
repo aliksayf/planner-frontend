aab20ef0570248f4beb7368e84bcfd24fa2d9059
aliksayf <52054450+aliksayf@users.noreply.github.com>
2024-05-14 18:24:44 -0700

    statistics

--- src/app/i/page.tsx
+++ src/app/i/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 
 import { NO_INDEX_PAGE } from '@/constants/seo.constants'
+import { Heading } from '@/components/ui/Heading'
+import { Statistics } from './Statistics'
 
 export const metadata: Metadata = {
 	title: 'Dashboard', 
@@ -8,5 +10,8 @@ export const metadata: Metadata = {
 }
 
 export default function DashboardPage() {
-	return <div>Dashboard</div>
+	return <div>
+		<Heading title='Statistics' />
+		<Statistics />
+	</div>
 }
